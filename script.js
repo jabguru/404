da60ef08cd2a4f24f249b13cfce5c4963e7d669f
Alibrown Julius <brownjulius980@gmail.com>
2019-07-05 00:15:17 +0100

    adjusted counter

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var heading = document.querySelector(".heading");
 var number = document.querySelector(".heading").textContent;
 
-for (let i = 50000; i >= number; i--) {
+for (let i = 9000; i >= number; i--) {
     window.setTimeout(function(){
         heading.textContent = i;
     }, 4000);
