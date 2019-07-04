f3781bcaebd18f0e51d28e5aa15e3aef682a6022
Alibrown Julius <brownjulius980@gmail.com>
2019-07-04 20:39:36 +0100

    edited js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var heading = document.querySelector(".heading");
+var number = document.querySelector(".heading").textContent;
 
-for (let i = 50000; i >= 404; i--) {
+for (let i = 50000; i >= number; i--) {
     window.setTimeout(function(){
         heading.textContent = i;
     }, 4000);
